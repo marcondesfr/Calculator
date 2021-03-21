2c2c1d26a862a145304e07168ece5456ad983e7c
Marcondes Ferreira <marcondessam@gmail.com>
2021-03-21 18:38:20 -0300

    updatecript

--- script/script.js
+++ script/script.js
@@ -1,6 +1,28 @@
-let num1 = 1;
-let num2 = 10;
-let res = res((n1 = 0, n2 = 0) => {
-    return n1 + n2
-})
-console.log(res(num1, num2))
+const display = document.querySelector(".ress"); //Select o Display.
+const numbers = document.querySelectorAll("[id*=button]"); //Select all Buttons.
+const operators = document.querySelectorAll("[id*=operators]")
+let newNumber = true;
+let operator;
+let beforeNumber;
+
+const insertNumber = (event) => updateDisplay(event.target.textContent); // it call updateDisplay's arrow function (Recebendo o texto contido no alvo do evento).
+
+numbers.forEach(numero => numero.addEventListener("click", insertNumber)); //Percorre todos os Ids com nome button[] add o evento Click.
+
+const updateDisplay = (text) => {
+    if (newNumber) {
+        display.textContent = text // add valores no display
+        newNumber = false;
+    } else {
+        display.textContent += text // concatena valores no display.
+    }
+};
+let selectOperator = () => {
+    if (!newNumber) {
+        newNumber = true;
+        operator = event.target.textContent;
+        beforeNumber = display.textContent;
+    }
+}
+
+operators.forEach(operator => operator.addEventListener("click", selectOperator)); //Percorre todos os Ids com nome operators[] add o evento Click.
